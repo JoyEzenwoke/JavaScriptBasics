8da7c4adfc7184a4fc635f251b803b90a1b1feb6
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-16 11:50:51 -1100

    001Varibles.js file created

---
+++ 001Varibles.js
@@ -0,0 +1,15 @@
+console.log("Hello world");
+
+let num = 6
+console.log(num);
+
+console.log(num + 3);
+
+num = 3
+console.log(num);
+
+let username = "Joy"
+console.log(username);
+
+username = "Joy's Account"
+console.log(username);n
