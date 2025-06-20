3a9eab1ead29f0e467acca76baf30aafc58110dd
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-20 07:00:15 -1100

    004RelationalOperators.js file created

---
+++ 005RelationalOperators.js
@@ -0,0 +1,52 @@
+//console.log(3 > 5);
+//console.log(7 > 6);
+let data = 3 > 5;
+console.log(data);
+let data1 = 7 <= 6;
+console.log(data1);
+
+let x = 7;
+let y = 7;
+let data2 = x >= y;
+console.log(data2);
+
+let a = "vehicle";
+let b = "bus";
+let data3 = a > b;
+console.log(data3);
+
+let c = "8";
+let d = 4;
+let data4 = c > d;
+console.log(data4);
+
+let e = 6;
+let f = 6;
+let data5 = e == f;
+console.log(data5);
+
+let g = "7";
+let h = 7;
+let data6 = g == h;
+console.log(data6);
+
+let i = "5";
+let j = 5;
+let data7 = i === j;
+console.log(data7);
+
+let k = '';
+let l = false;
+let data8 = k == l;
+console.log(data8);
+
+let m = '';
+let n = false;
+let data9 = m === n;
+console.log(data9);
+
+
+
+
+
+
---
+++ 006LogicalOperators.js
@@ -0,0 +1,9 @@
+let a = 14;
+let b = 6; 
+let c = 7;
+let data = a > b;
+let data1 = a > b && a > c;
+let data2 = a < b && a < c;
+let data3 = a > b || a > c;
+let x = !data3;
+console.log(data3, x);
