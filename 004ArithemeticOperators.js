9e350818a9e136d30ec5e2c474aae3175122f89f
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-20 06:46:32 -1100

    004ArithemeticOperators.js file created

---
+++ 004ArithemeticOperators.js
@@ -0,0 +1,41 @@
+let num1 = 17;
+let num2 = 5;
+
+let result = num1 + num2;
+console.log(result);
+let result2 = num1 - num2;
+console.log(result2);
+let result3 = num1 / num2;
+console.log(result3);
+let result4 = num1 * num2;
+console.log(result4);
+let result5 = num1 % num2;
+console.log(result5);
+
+let num3 = true;
+let num4 = true;
+let result6 = num3 + num4;
+console.log(result6);
+
+let num = 5;
+//num = num - 3;
+//num += 3;
+//num++;(post increment)
+//++num;(pre increment)
+//let x = num++;
+//let x = ++num;
+//console.log(x, num);
+//let result7 = num * num * num;
+//console.log(result7);
+//let result8 = Math.pow(5,3);
+//console.log(result8);
+let result9 = 5**3;
+console.log(result9);
+
+
+
+
+
+
+
+
