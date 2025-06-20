8e3c749f0832001b5b99127f7a95dbba888d709e
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-20 06:37:59 -1100

    003TypeConversion.js file created

---
+++ 003TypeConversion.js
@@ -0,0 +1,34 @@
+let num1 = 3;
+console.log(num1, typeof num1);
+
+let num2 = "6" + 5;
+console.log(num2, typeof num2);
+
+let num3 = "12" - 10;
+console.log(num3, typeof num3);
+
+let num4 = true + 1;
+console.log(num4, typeof num4);
+
+let num5 = null + 9;
+console.log(num5, typeof num5);
+
+let str = "125";
+let num = Number(str);
+console.log(num, typeof num);
+
+let number = 456;
+let str1 = String(number);
+console.log(str1, typeof str1);
+
+let bool = true;
+let str2 = String(bool);
+console.log(str2, typeof str2);
+
+let stringvalue = "hello";
+let bool2 = Boolean(stringvalue);
+console.log(bool2, typeof bool2);
+
+x = 9;
+x = !x;
+console.log(x, typeof x);
