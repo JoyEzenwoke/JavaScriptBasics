698ea69d261a6abd37c25edc7f6b1d819112fecd
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-21 06:22:50 -1100

    010TemplateLiteral.js file created

---
+++ 010TemplateLiteral.js
@@ -0,0 +1,10 @@
+let num1 = 6;
+let num2 = 6;
+let output = num1 + num2;
+
+console.log(`addition of ${num1} and ${num2} is ${output}`);
+console.log(`My name is 
+Joy Ezenwoke`);
+//console.log(output);
+//console.log("addition of 6 and 6 is 12");
+//console.log(" addition of " + num1 + " and " + num2 + " is " + output);
