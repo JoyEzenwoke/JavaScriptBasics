7b2e66ce0b0cb4323b72b2d87919017e92c0667f
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-06-21 03:06:35 -1100

    007ConditionalStatement.js file created

---
+++ 007ConditionalStatement.js
@@ -0,0 +1,24 @@
+let num1 = 6
+let num2 = 4
+
+let result = num2 < num1
+
+if (result)
+
+console.log("num2 is less than num1");
+
+console.log("Byee.....");
+
+let num3 = 9
+let num4 = 7
+let num5 = 4
+
+if (num3 > num4 && num3 > num5){
+    console.log("num3 is greatest");
+}
+
+else if (num4 < num5 && num4 > num3)
+    console.log("num4 is greater")
+
+else 
+    console.log("num5 is greatest")
---
+++ 008TernaryOperators.js
@@ -0,0 +1,30 @@
+let n = 2
+if(n/2===0);
+    console.log("even")
+
+let num = 7
+
+let result
+if(num%2===0)
+    result="even"
+
+else
+    result = "odd"
+
+console.log(result);
+console.log("Exit....")
+
+let a = 12
+let output 
+//if(a%2===0)
+    //output="even"
+
+//else
+    //output = "odd"
+
+    output = a%2===0 ? "Even" : "Odd"
+    console.log(output);
+    console.log("Exit....")
+
+
+    
