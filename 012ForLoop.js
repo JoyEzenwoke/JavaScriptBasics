d4b94ebf7d52a5d42cb2a838b3448e95f757c05f
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-07-03 06:38:03 -1100

    010TemplateLiteral.js file created

---
+++ 012ForLoop.js
@@ -0,0 +1,12 @@
+// repeat this statement 5 times
+// initialize, condition, increment
+
+
+for (let i = 1;i<=4;i++)
+{
+    console.log("Hello",i);
+
+    for (let a = 1;a<=5;a++) 
+        console.log("Hello",a);
+    
+}
