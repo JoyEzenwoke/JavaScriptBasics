1f9467bd7d093f3dc8cd79abe63e836e3c51f9bb
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-07-03 07:34:43 -1100

    013Objects.js file created

---
+++ 013Objects.js
@@ -0,0 +1,9 @@
+let visitors = {
+     names: "favour , mercy",
+     tech: "js" ,
+     favourite_food: "fried rice"
+}
+
+console.log(visitors);
+console.log(visitors.names);
+console.log(visitors["favourite_food"])
---
+++ 014ComplexObjects.js
@@ -0,0 +1,17 @@
+let friends = {
+    names: "wisdom , sochima",
+    hobby: "reading",
+    
+    laptop: {
+        cpu: "i5",
+        ram: 4,
+        brand: "hp"
+    }
+}
+  delete friends.hobby
+ console.log(friends);
+ console.log(friends.names);
+ console.log(friends.laptop);
+ console.log(friends.laptop.brand);
+ 
+ 
