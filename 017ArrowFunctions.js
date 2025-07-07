26b7b3c64cc9e6c146d1e21d001a551679734cbb
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-07-06 19:04:28 -1100

    017ArrowFunctions.js file created

---
+++ 017ArrowFunctions.js
@@ -0,0 +1,12 @@
+let greet = (user) => {
+    console.log("Hello " + user);
+    return 2; 
+}
+
+console.log(greet('Joy'));
+// this sign(=>) means the arrow function
+let add = (num1,num2) => num1 + num2;
+//when you have one statement you dont need a curly bracket.
+
+let output = add(3,6)   
+console.log(output);
